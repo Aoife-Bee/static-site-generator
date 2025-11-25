c96225f175635ebb026d33bc9ffd569bb0842e49
Aoife Bee <aoifebeeof@gmail.com>
2025-11-24 19:58:26 -0500

    adjusted gencontent to debug uncopied index on github.io

--- src/gencontent.py
+++ src/gencontent.py
@@ -16,10 +16,15 @@ def generate_page(from_path, template_path, dest_path, base_path="/"):
         contents = f.read()
     with open(template_path, "r") as t:
         template = t.read()
+
     html_node = markdown_to_html_node(contents)
     html_string = html_node.to_html()
     title = extract_title(contents)
-    output = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    output = template.replace("{{ Title }}", title)
+    output = output.replace("{{ Content }}", html_string)
+    output = output.replace('href="/', f'href="{base_path}')
+    output = output.replace('src="/', f'src="{base_path}')
+
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as d:
         d.write(output)
@@ -28,8 +33,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, bas
     for item in os.listdir(dir_path_content):
         item_path = os.path.join(dir_path_content, item)
         if os.path.isdir(item_path):
-            generate_pages_recursive(item_path, template_path, os.path.join(dest_dir_path, item))
-        if item.endswith(".md"):
+            generate_pages_recursive(item_path, template_path, os.path.join(dest_dir_path, item), base_path)
+        elif item.endswith(".md"):
             base_name = os.path.splitext(item)[0] + ".html"
             dest_path = os.path.join(dest_dir_path, base_name)
-            generate_page(item_path, template_path, dest_path)
+            generate_page(item_path, template_path, dest_path, base_path)
